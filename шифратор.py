48e7d9ffcd43e614c4d7461b06b8849a64f7e8ee
mishayar <misha.yar.1999@gmail.com>
2018-02-03 10:35:27 +0300

    fix

--- шифратор.py
+++ шифратор.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # coding: utf-8
 
-step = 4
-alf = u'АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя'
+from lib import step , alf
+
 s1 = input('Введите текст: ')
 res = u''
 for c in s1:
